8f65f85fe6314526d99ebd95534e294d9f7c1d60
Shawn Liu Han Sheng <65820645+Cuebitar@users.noreply.github.com>
2023-07-13 12:27:30 +0800

    Complete setup env

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,10 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
     plugins: [
-        laravel({
-            input: [
-                'resources/sass/app.scss',
-                'resources/js/app.js',
-            ],
-            refresh: true,
-        }),
         vue({
             template: {
                 transformAssetUrls: {
@@ -23,6 +17,7 @@ export default defineConfig({
     resolve: {
         alias: {
             vue: 'vue/dist/vue.esm-bundler.js',
+            '@': fileURLToPath(new URL('./src', import.meta.url))
         },
     },
 });
