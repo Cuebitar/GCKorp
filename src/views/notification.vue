d83b53fac786d0b4bb762fcb533ab0a9c545b664
kokmin912 <kokminlim@gmail.com>
2023-08-14 09:09:46 +0800

    lim

--- src/views/notification.vue
+++ src/views/notification.vue
@@ -10,7 +10,6 @@
                     <div class="col-12 grid mt-2 pb-2 dataViewCustom">
                         <span class="col-2">{{ slotProps.data.date }}</span>
                         <span class="col-6">{{ slotProps.data.label }}</span>
-                        <span class="col-1">{{ slotProps.data.type }}</span>
                         <span v-if="slotProps.data.status != null" class="col-2 text-center">{{ slotProps.data.status }}</span>
                         <span v-else class="col-2"></span>
                         <span v-if="slotProps.data.amount != null" class="col-1 ">MYR {{ slotProps.data.amount}}</span>
@@ -43,27 +42,23 @@ export default {
                 date: '2/7/2023',
                 label: 'Transaction ref 11111111111',
                 status: 'Successful',
-                type: 'deposit',
                 amount: 5000,
             },
             {
                 date: '4/7/2023',
                 label: 'Transaction ref 22222222222',
                 status: 'Successful',
-                type: 'deposit',
                 amount: 5000,
             },
             {
                 date: '5/7/2023',
                 label: 'Dividen distribition',
-                type: 'dividen',
                 amount: 5000,
             },
             {
                 date: '5/7/2023',
                 label: 'Transaction ref 333333333333',
                 status: 'Fail',
-                type: 'withdrawal',
                 amount: 5000,
             },
            
