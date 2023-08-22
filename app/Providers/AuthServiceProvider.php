8468dcdf3949bad641a3d184c2d6a7cb82aad2c0
Cuebitar <shawnlhs01@gmail.com>
2023-08-23 03:06:38 +0800

    Finish registeration controller

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -22,7 +22,6 @@ class AuthServiceProvider extends ServiceProvider
     public function boot(): void
     {
         $this->registerPolicies();
-        //Passport::loadKeysFrom('/secret-keys/oauth');
         ResetPassword::createUrlUsing(function (object $notifiable, string $token) {
             return config('app.frontend_url')."/password-reset/$token?email={$notifiable->getEmailForPasswordReset()}";
         });
