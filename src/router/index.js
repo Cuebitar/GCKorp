2601af6d761eda2ca8fa811ffa4dd8886616b04a
kokmin912 <kokminlim@gmail.com>
2023-08-29 11:36:17 +0800

    admin notification and transaction

---
+++ src/router/index.js
@@ -0,0 +1,111 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHashHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/devExample',
+      name: 'example',
+      component: () => import('../views/exampleView.vue'),
+    },
+
+    {
+      path: '/profile',
+      name: 'profile',
+      component: () => import('../views/profileView.vue'),
+    },
+
+    {
+      path: '/logout',
+      name: 'logout',
+      component: () => import('../views/logoutView.vue'),
+    },
+
+    {
+      path: '/registerPersonal',
+      name: 'registerPersonal',
+      component: () => import('../views/registerPersonalView.vue'),
+    },  
+    
+    {
+      path: '/registerBank',
+      name: 'registerBank',
+      component: () => import('../views/registerBankView.vue'),
+    },
+
+    {
+      path: '/',
+      name: 'home',
+      component: () => import('../views/homeView.vue'),
+    },
+
+    {
+      path: '/dashboard',
+      name: 'User Dashboard',
+      component: () => import('../views/userDashboardView.vue'),
+    },
+
+    {
+      path: '/statement',
+      name: 'Search Statement',
+      component: () => import('../views/searchStatementView.vue'),
+    },
+    {
+      path: '/transaction',
+      name: 'Transaction',
+      component: () => import('../views/transactionView.vue'),
+    },
+    {
+      path: '/favourite',
+      name: 'Favourite Account',
+      component: () => import('../views/favouriteView.vue'),
+    },
+    {
+      path: '/favouriteadd',
+      name: 'Add Favourite Account',
+      component: () => import('../views/favouriteAddView.vue'),
+    },
+    {
+      path: '/registerBank',
+      name: 'registerBank',
+      component: () => import('../views/registerBankView.vue'),
+    },
+    {
+      path: '/deposit',
+      name: 'Deposit',
+      component: () => import('../views/depositView.vue'),
+    },
+    {
+      path: '/withdrawal',
+      name: 'Withdrawal',
+      component: () => import('../views/withdrawalView.vue'),
+    },
+    {
+      path: '/notification',
+      name: 'Notification',
+      component: () => import('../views/notificationView.vue'),
+    },
+    {
+      path: '/AdminNotification',
+      name: 'AdminNotification',
+      component: () => import('../views/adminNotificationView.vue'),
+    },
+    {
+      path: '/AdminTransaction',
+      name: 'AdminTransaction',
+      component: () => import('../views/adminTransactionView.vue'),
+    },
+    {
+      path: '/adminWithdrawalList',
+      name: 'AdminWithdrawalListView',
+      component: () => import('../views/adminWithdrawalListView.vue'),
+    },
+    //*{
+    //  path: '/about',
+    //  name: 'about',
+    //  component: () => import("")
+    //}
+  ]
+})
+
+export default router
---
+++ src/views/adminNotificationView.vue
@@ -0,0 +1,73 @@
+<template>
+    <UniHeader ></UniHeader>
+    <div class="content">
+
+        <!--Notification Content-->
+        <div>
+            <h1 class="font-bold">Notification</h1>
+            <DataView :value="notification" paginator :rows="10" class="shadow-5 mt-4 ">
+                <template #list="slotProps">
+                    <div class="col-12 grid mt-2 pb-2 dataViewCustom">
+                        <span class="col-2">{{ slotProps.data.date }}</span>
+                        <span class="col-6">{{ slotProps.data.label }}</span>
+                        <span v-if="slotProps.data.status != null" class="col-2">{{ slotProps.data.status }}</span>
+                        <span v-else class="col-2"></span>
+                        <span v-else class="col-1"></span>
+
+
+
+                </div>
+                </template>
+            </DataView>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+   data() {
+    return {
+//notification dummy data
+        notification:[
+            {
+                date: '1/7/2023',
+                label: 'Deposit transaction ref 111111111',
+                status: 'Denied',
+            },
+            {
+                date: '1/7/2023',
+                label: 'Withdrawal transaction ref 111111111',
+                status: 'Approved',
+            },
+            {
+                date: '2/7/2023',
+                label: 'Deposit transaction ref 2222222222',
+                status: 'Approved',
+            },
+            {
+                date: '4/7/2023',
+                label: 'Withdrawal transaction ref 3333333333',
+                status: 'Denied',
+            },
+            {
+                date: '5/7/2023',
+                label: 'Withdrawal transaction ref 5555555555',
+                status: 'Approved',
+            },
+            {
+                date: '5/7/2023',
+                label: 'Deposit transaction ref 5555555555',
+                status: 'Denied',
+            },
+           
+        ],  
+    }
+   }, 
+}
+</script>
+
+<style lang="scss" scoped>
+.dataViewCustom{
+    margin-left: inherit;
+}
+</style>
